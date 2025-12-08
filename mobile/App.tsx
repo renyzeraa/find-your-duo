933f82c512ebf64614c812ca72525f7a87e5dbe4
Renan Silva <renansilvaytB@gmail.com>
2025-12-08 19:30:27 -0300

    fix: typescript errors

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef } from "react";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter'
+import { EventSubscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications';
+
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+
+import './src/service/notificationConfigs';
+import { getPushNotificationToken } from "./src/service/getPushNotificationToken";
 
 export default function App() {
+  const getNotificationListener = useRef<EventSubscription | null>(null);
+  const responseNotificationListener = useRef<EventSubscription | null>(null);
+
+  useEffect(() => {
+    getPushNotificationToken()
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener((notification: any) => {
+      console.log(notification)
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener((response: any) => {
+      console.log(response)
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
